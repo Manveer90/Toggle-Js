e48d3ffb1f1e9b2607b87469f2cdc12206b8a70f
Manveer90 <mnveer89@gmail.com>
2025-07-26 12:30:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const togglebg = document.getElementById('toggleDark')
+const body = document.querySelector('body')
+const icon = document.getElementById('icon')
+const password = document.getElementById('password')
+const box = document.getElementById('box')
+const card = document.getElementById('card')
+const text = document.getElementById('text')
+
+togglebg.addEventListener('click' , function(){
+    this.classList.toggle('bi-moon')
+    if (this.classList.toggle('bi-brightness-high-fill')) {
+        body.style.background = 'white'
+        body.style.color = 'black'
+        body.style.transition = '2s'
+        password.style.background = 'white'
+        box.style.border = '2px solid black'
+        text.textContent = 'JavaScript Toggle Light Mode'
+    }else{
+       body.style.background = 'black'
+        body.style.color = 'white'
+        body.style.transition = '2s'
+        box.style.border = '2px solid white'
+        text.textContent = 'JavaScript Toggle Dark Mode'
+    }
+})
+
+icon.addEventListener('click' , function () {
+      if(password.type === 'password'){
+        password.type = 'text'
+        this.src = 'eye-open.png'
+    }else{
+        password.type = 'password'
+        this.src = 'eye-close.png'
+    }
+})
+
+card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+});
+
+
